edcc7a0d8450b2088dae5d1e8c35b5da024e46bf
Shayan Ghani <shayanghani1384@gmail.com>
2024-12-27 12:46:12 +0330

    chore(db): add shema sql files

---
+++ internal/db/Migrations/000001_init_schema.up.sql
@@ -0,0 +1,44 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for item_tags
+-- ----------------------------
+DROP TABLE IF NOT EXISTS`item_tags`;
+CREATE TABLE `item_tags` (
+  `item_id` bigint unsigned NOT NULL,
+  `tag_id` int unsigned NOT NULL,
+  PRIMARY KEY (`item_id`,`tag_id`),
+  KEY `idx_item_tag_tag_id` (`tag_id`),
+  KEY `idx_item_tag_item_id` (`item_id`) USING BTREE,
+  CONSTRAINT `fk_item_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE
+)
+
+-- ----------------------------
+-- Table structure for items
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(155) NOT NULL,
+  `description` mediumtext,
+  `is_done` bit(1) DEFAULT b'0',
+  `created_at` datetime(3) NOT NULL,
+  `modified_at` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_item_id` (`id`) USING BTREE /*!80000 INVISIBLE */
+)
+
+-- ----------------------------
+-- Table structure for tags
+-- ----------------------------
+DROP TABLE IF NOT EXISTS `tags`;
+CREATE TABLE `tags` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `created_at` datetime(3) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_tag_name_index` (`name`)
+)
+
+SET FOREIGN_KEY_CHECKS = 1;
